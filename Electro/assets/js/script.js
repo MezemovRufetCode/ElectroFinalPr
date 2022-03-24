b8074b77de5aa274b1d2a3a422ef6e1c2b5aa575
MezemovRufetCode <rufatam@code.edu.az>
2022-03-25 02:59:41 +0400

    BlogandBlogDetailpageReady

--- Electro/assets/js/script.js
+++ Electro/assets/js/script.js
@@ -492,4 +492,17 @@ $(document).ready(function () {
     $(this).next(".answer").slideToggle(200);
   });
   // ---Ending FAQ page-------
+
+  $(".topics a").on("click", function () {
+    //selecting the syllabus class
+    $select = $('<div class="syllabus"></div>');
+    $(this)
+      .parents("li")
+      .each(function (n, li) {
+        //Adding / to each anchor tag of li
+        $select.prepend(" > ", $(li).children("a").clone());
+      });
+    // Displaying the hierarchical order of pages.
+    $(".display").html($select.prepend('<a href="#syllabus">Home</a>'));
+  });
 });
